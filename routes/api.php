55ce78b1da5ac0f9adee52cafbff08419d67a35f
dzackiero <dzakynashshar@gmail.com>
2024-05-27 08:52:14 +0700

    fix api controller

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource("transactions", \App\Http\Controllers\TransactionController::class);
-Route::apiResource("trashes", \App\Http\Controllers\TransactionController::class);
-Route::apiResource("members", \App\Http\Controllers\TransactionController::class);
+Route::apiResource("trashes", \App\Http\Controllers\TrashController::class);
+Route::apiResource("members", \App\Http\Controllers\MemberController::class);
